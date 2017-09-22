c73743c90919723e7f8a259dc478d6b3b32dddba
Gabriel Kirsten Menezes <gabriel.kirsten@hotmail.com>
2017-09-22 13:37:51 -0400

    fix script, new function shuffle

--- utils/script_convertall.sh
+++ utils/script_convertall.sh
@@ -1,34 +1,33 @@
 #!/bin/bash
+
 #
-#    Script - convert tif to png
+#    Script - split data between train and test
 #
-#    Name: script_convertall.sh
+#    Name: script_split_data.sh
 #    Author: Gabriel Kirsten Menezes (gabriel.kirsten@hotmail.com)
 #
- 
-echo "[SCRIPT CONVERT ALL] Initializing..."
- 
+
+echo "[SCRIPT SPLIT DATA] Initializing..."
+
 dir_train="../data/train"
 dir_validation="../data/validation"
- 
-echo "[SCRIPT CONVERT ALL] Converting train..."
- 
+perc_train=70
+
 for dir_class in `ls $dir_train`;
 do
-    echo "[SCRIPT CONVERT ALL] Converting class -" $dir_class;
-    convert $dir_train/$dir_class/*.tif $dir_train/$dir_class/image-$counter.png 
-    echo "[SCRIPT CONVERT ALL] Removing all .tif files in $dir_class ..."
-    rm $dir_train/$dir_class/*.tif
+    echo "[SCRIPT SPLIT DATA] Spliting class -" $dir_class;
+    mkdir $dir_validation/$dir_class
+    quantity_files=`ls $dir_train/$dir_class | wc -l`
+    perc_quantity_files=$((($quantity_files/100)*$perc_train))
+    counter=0
+    arrayFiles=`ls $dir_train/$dir_class |sort -R`
+    for file in $arrayFiles;
+    do
+        let "counter += 1"
+        if [[ $counter -gt $perc_quantity_files ]]; then
+            mv $dir_train/$dir_class/$file $dir_validation/$dir_class/$file
+        fi
+    done
 done
- 
-echo "[SCRIPT CONVERT ALL] Converting validation..."
- 
-for dir_class in `ls $dir_validation`;
-do
-    echo "[SCRIPT CONVERT ALL] Converting class -" $dir_class;
-    convert $dir_train/$dir_class/*.tif $dir_validation/$dir_class/image-$counter.png &
-    echo "[SCRIPT CONVERT ALL] Removing all .tif files in $dir_class ..."
-    rm $dir_validation/$dir_class/*.tif
-done
- 
-echo "[SCRIPT CONVERT ALL] OK! DONE."
+
+echo "[SCRIPT SPLIT DATA] OK! DONE."
